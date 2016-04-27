8eeb1ea44156fa3151228b46f583a89a19b4bf59
Keith Moore <keith.m4@gmail.com>
2016-04-27 14:53:15 -0400

    connected electric database and ng-repeat results

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,11 +1,37 @@
 var express = require('express');
 var router = express.Router();
-
+var mongoClient = require('mongodb').MongoClient;
+var mongoUrl = 'mongodb://localhost:27017/electric';
+var db;
 // removed router.get because we will never use it for this app
+// create a connection to mongo
+mongoClient.connect(mongoUrl, function(error, database){
+	db = database;
+});
+
+// if someone puts /search into the address bar, render all cars as the result
+router.get('/search', function(req, res, next) {
+	db.collection('cars').find().toArray(function(error, result){
+		res.json(result);
+	});
+});
 
 router.post('/search', function(req, res, next) {
-	res.json({
-		message: "success"
+	var myCar = req.body.name;
+	if (req.body.name.length < 1){
+		res.status(500);
+		return res.render('User input error.');
+	}
+	// query the electric database
+	db.collection('cars').find({name: myCar}).toArray(function(error, result){
+		if (result.length < 1){
+			db.collection('cars').insertOne({name: myCar});
+			db.collection('cars').find({name: myCar}).toArray(function(error, result2){
+				res.json(result2);
+			});
+		} else {
+			res.json(result);
+		}
 	});
 });
 
--- front-end/controller.js
+++ front-end/controller.js
@@ -1,22 +1,43 @@
 var searchApp = angular.module('searchApp', []);
 
 searchApp.controller('searchController', function($scope, $http){
-	$scope.search = function(){
-		// how do we handle post requests to this URL? see ../back-end/routes/index.js 
-		var apiUrl = 'http://localhost:3000/search';
+	// how do we handle requests to this URL? see ../back-end/routes/index.js 
+	var apiUrl = 'http://localhost:3000/search';
+	
+	function getAllCars(){
+		// make an http get request that returns all the cars
+		$http({
+			method: 'GET',
+			url: apiUrl,
+		}).then(function successCallback(response){
+			if (response.data.length < 1){
+				$scope.results = ['No results.'];
+			} else {
+				$scope.results = response.data;
+			}
+		}, function errorCallback(status){
+			$scope.results = 'Error: ' + status;
+		});
+	}
 
+	// on load, get all cars in the database
+	getAllCars();
+
+	$scope.search = function(){
 		// make an http post request with your name in the header
 		// on sucess, set the status and result for the scope
 		$http({
 			method: 'POST',
 			url: apiUrl,
-			data: {name: "Keith"}
+			data: {name: $scope.carToAdd}
 		}).then(function successCallback(response){
-				$scope.status = response.status;
-				$scope.result = response.config.data.name;
-			}, function errorCallback(status){
-				$scope.status = 500;
-				$scrope.result = 'error';
-			});
-		}
+			if (response.data.length < 1){
+				$scope.results = ['No results.'];
+			} else {
+				$scope.results = response.data;
+			}
+		}, function errorCallback(status){
+			$scope.results = 'Error: ' + status;
+		});
+	}
 });
